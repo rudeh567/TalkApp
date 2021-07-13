bff9906585aaabe0d268f017ba28199989aa2b1c
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-07-13 21:00:58 +0900

    HowTalk Project
    프로젝트 생성

---
+++ HowTalk/HowTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HowTalk
+//
+//  Created by GSM08 on 2021/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
