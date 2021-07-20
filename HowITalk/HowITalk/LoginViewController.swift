d8e6690abb587be5d7689651f7f33f758c18bac6
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-07-20 22:55:45 +0900

    TabBar

--- HowITalk/HowITalk/LoginViewController.swift
+++ HowITalk/HowITalk/LoginViewController.swift
@@ -42,7 +42,7 @@ class LoginViewController: UIViewController {
         
         Auth.auth().addStateDidChangeListener { (auth, user) in
             if user != nil {
-                let view = self.storyboard?.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+                let view = self.storyboard?.instantiateViewController(withIdentifier: "MainViewTabBarController") as! UITabBarController
                 self.present(view, animated: true, completion: nil)
             }
         }
