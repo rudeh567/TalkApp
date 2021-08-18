695007b4a6674c0a8d939f1e796e39e16a1c399b
rudeh567 <49026505+rudeh567@users.noreply.github.com>
2021-08-18 17:18:08 +0900

    chat view

--- HowITalk/HowITalk/ChatViewController.swift
+++ HowITalk/HowITalk/ChatViewController.swift
@@ -14,6 +14,11 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        view.addSubview(tableView)
         
     }
     
